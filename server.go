95f00c3f5441c9f1ddf4f8f134eb61b2d9aa7850
Ox Cart <ox.to.a.cart@gmail.com>
2014-11-07 17:00:13 -0600

    Removed cipher suites

--- server.go
+++ server.go
@@ -15,23 +15,6 @@ const (
 	DefaultNumBuffers = 10000
 )
 
-var (
-	// The ECDHE cipher suites are preferred for performance and forward
-	// secrecy.  See https://community.qualys.com/blogs/securitylabs/2013/06/25/ssl-labs-deploying-forward-secrecy.
-	preferredCipherSuites = []uint16{
-		tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
-		tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
-		tls.TLS_RSA_WITH_RC4_128_SHA,
-		tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
-		tls.TLS_RSA_WITH_AES_128_CBC_SHA,
-		tls.TLS_RSA_WITH_AES_256_CBC_SHA,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-	}
-)
-
 // Server is a waddell server
 type Server struct {
 	// NumBuffers: number of buffers to cache for reading and writing (balances
