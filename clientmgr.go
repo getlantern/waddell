76b428aed11807bde589528a4a918a10205fa54d
Ox Cart <ox.to.a.cart@gmail.com>
2014-11-26 21:08:16 -0600

    Added ClientMgr

---
+++ clientmgr.go
@@ -0,0 +1,64 @@
+package waddell
+
+import (
+	"net"
+	"sync"
+)
+
+// IdCallback is a callback for id (re)assignments for the client connected to
+// the indicated addr.
+type IdCallback func(addr string, id PeerId)
+
+// ClientMgr provides a mechanism for managing connections to multiple waddell
+// servers.
+type ClientMgr struct {
+	// Dial is a function that dials the waddell server at the given addr.
+	Dial func(addr string) (net.Conn, error)
+
+	// ReconnectAttempts specifies how many consecutive times to try
+	// reconnecting in the event of a connection failure. See
+	// Client.ReconnectAttempts for more information.
+	ReconnectAttempts int
+
+	// IdCallback optional allows specifying a callback that gets notified
+	// everytime a client is assigned a new PeerId
+	IdCallback IdCallback
+
+	clients      map[string]*Client
+	ids          map[string]PeerId
+	clientsMutex sync.Mutex
+}
+
+func (m *ClientMgr) ClientTo(addr string) (*Client, PeerId, error) {
+	m.clientsMutex.Lock()
+	defer m.clientsMutex.Unlock()
+	if m.clients == nil {
+		m.clients = make(map[string]*Client)
+		m.ids = make(map[string]PeerId)
+	}
+	client := m.clients[addr]
+	if client == nil {
+		client = &Client{
+			Dial: func() (net.Conn, error) {
+				return m.Dial(addr)
+			},
+			ReconnectAttempts: m.ReconnectAttempts,
+		}
+		id, err := client.Connect()
+		if err != nil {
+			return nil, id, err
+		}
+		m.clients[addr] = client
+		m.ids[addr] = id
+		if m.IdCallback != nil {
+			m.IdCallback(addr, id)
+			go func() {
+				for id := range client.UpdatedIdsCh {
+					m.IdCallback(addr, id)
+				}
+			}()
+		}
+	}
+	id := m.ids[addr]
+	return client, id, nil
+}
